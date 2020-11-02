ef7ff87c957c8571dd7e1edc5ae5f9f526480a9e
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-02 14:27:17 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -12,7 +12,7 @@ def is_prime(num):
 
 # iterate till we get the solution
 print("Getting you the 10001 th prime number. It may take upto 1min, Please be paitent.")
-while count < 10001:
+while count < 10002:
     count += 1 if is_prime(num) else 0
     num += 1
 
