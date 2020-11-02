eab1e4b64a1646ef1423a8780dbd871a606de60f
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-02 13:53:29 +0530

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,8 +1,9 @@
+# reverse the values
 num1 = 0
 num2 = 0
 final = 0
 
-
+# get the reverse of number
 def reverse(num):
     rev = 0
     while (num >= 1):
@@ -11,7 +12,7 @@ def reverse(num):
         num = num // 10
     return rev
 
-
+# loop i and j to find the solution
 for i in range(100, 1000):
     for j in range(100, 1000):
         p = i * j
@@ -22,5 +23,6 @@ for i in range(100, 1000):
             num2 = j
             final = p
 
+# print the result
 print(
     f"The largest palindrome number made from the product of two 3-digit numbers is {final}. It is the product of {num1} and {num2}.")
