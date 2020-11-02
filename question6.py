619003c51cb31430989833dc84151eadd5e6174c
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-02 13:54:44 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,16 +1,20 @@
+# reserve values
+count = 0
+num = 2
+
+
+# check prime number
 def is_prime(num):
     for i in range(2, num):
         if (num % i) == 0:
             return False
     return True
 
-
-count = 0
-num = 2
-
+# iterate till we get the solution
 print("Getting you the 10001 th prime number. It may take upto 1min, Please be paitent.")
 while count < 10001:
     count += 1 if is_prime(num) else 0
     num += 1
 
+# print the results
 print(f"The 10001th prime number is {num - 1}")
