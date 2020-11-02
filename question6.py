ebc1905c2d28e238311163677dc17ef97da04b1f
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-02 13:15:41 +0530

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,1 +1,15 @@
+def is_prime(num):
+    for i in range(2, num):
+        if (num % i) == 0:
+            return False
+    return True
 
+
+count = 0
+num = 2
+
+while count < 10001:
+    count += 1 if is_prime(num) else 0
+    num += 1
+
+print(f"The 10001th prime number is {num - 1}")
