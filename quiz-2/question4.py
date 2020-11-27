13ce295d43939d05f0d1d50fbc0662ea1599ed5d
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-27 19:46:53 +0530

    Update question4.py

--- quiz-2/question4.py
+++ quiz-2/question4.py
@@ -2,14 +2,13 @@ N = input("Enter size of matrix > ")
 MATRIX = mat = [[0 for i in range(N)] for i in range(N)]
 
 for i in range(N):
-        mat[i][i] = 1
+        MATRIX[i][i] = 1
     for i in range(N):
-        mat[i][N - i - 1] = 1
+        MATRIX[i][N - i - 1] = 1
     if (N % 2 != 0):
-        mat[N // 2][0] = 1
-        mat[0][N // 2] = 1
+        MATRIX[N // 2][0] = 1
+        MATRIX[0][N // 2] = 1
     for i in range(N):
         for j in range(N):
-            print(mat[i][j], end = " ")
-             
-        print()
+            print(MATRIX[i][j], end = " ")             
+        print("")
