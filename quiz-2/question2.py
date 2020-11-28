e00e80d20f8b7ae511ff9cb9f70aa44317270ef0
Shreejoy Dash <shreejoy@pixelexperience.org>
2020-11-28 12:20:54 +0530

    Update question2.py

--- quiz-2/question2.py
+++ quiz-2/question2.py
@@ -1,20 +1,36 @@
+import sys
+
+"""
+variable TMO represents temprature of the room in degree C
+variable NDO represents the number of times the door was opened
+variable RT represents the time for which purifier was operational.
+variable EFF represents the effieceny changed due to room temprature
+"""
+
+# get the needed values and convert to int
 try:
-    temp = int(input("Temperature in deg C > "))
-    d_open = int(input("No of times door opened > "))
-    run_time = int(input("Time for which the purifier is turned on > "))
+    RT = int(input("Purifier's working time (minutes) > "))
+    NDO = int(input("Count of times door opened (int) > "))
+    TMP = int(input("Temperature of room in degree (C) > "))
 except Exception as e:
     print(f"Invalid input: {e}")
 
-if d_open > run_time:
-    raise Exception("The door can be opened once a minute."
-                    + " So the number of times door opened should be"
-                    + " numerically less than or equals to run time")
 
-eff = int((temp - 25) * 2)
-total = (run_time - d_open) * (40 + eff) + d_open * (30 + eff)
+"""
+The door can be opened once a minute so its not possible to have the amount of running duration to be 
+numerically less than the number of times the door was opened.
+"""
+if NDO > RT:
+    raise sys.exit("Not a possible scenario. Please try again with correct inputs")
 
+
+# Calculate the effect on efficency on purifier due to the the 
+EFF = int((TMP - 25) * 2)
+# Calculate the required value
+total = (RT - NDO) * (40 + EFF) + NDO * (30 + EFF)
+
+# If the value is 0 or less then it mean the purifier failed to do any work.
 if total <= 0:
-    raise Exception(
-        "Under the given sitution the purifier will not work at all")
+    sys.exit("Under the given sitution the purifier will not work at all")
 else:
     print(f"The total amount purfied is {total}")
