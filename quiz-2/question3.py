47d3fb04fa30ebb9230d246444fda94c984f47e9
1941012973 <1941012973.g.shreejoydash@gmail.com>
2020-11-29 04:59:41 +0000

    Format py files

--- quiz-2/question3.py
+++ quiz-2/question3.py
@@ -5,11 +5,11 @@ B = input("B > ")
 N = input("N > ")
 
 for i in range(N):
-  if X <= A and Y <= B:
-    break
-    
-  if X > A:
-    X -= 1
-   
-  if Y > Y:
-    Y -= 1
+    if X <= A and Y <= B:
+        break
+
+    if X > A:
+        X -= 1
+
+    if Y > Y:
+        Y -= 1
