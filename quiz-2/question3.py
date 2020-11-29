f289e10ff88ebe79c51060e1c7419468f0c4beb8
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2020-11-29 10:35:08 +0530

    Update question3.py

--- quiz-2/question3.py
+++ quiz-2/question3.py
@@ -1,15 +1,25 @@
-X = input("X > ")
-Y = input("Y > ")
-A = input("A > ")
-B = input("B > ")
-N = input("N > ")
+import sys
+
+X = int(input("X > "))
+Y = int(input("Y > "))
+A = int(input("A > "))
+B = int(input("B > "))
+N = int(input("N > "))
+
+if (X - A + Y - B) < N:
+    sys.exit('e1')
 
 for i in range(N):
     if X <= A and Y <= B:
+        print('e2')
         break
 
     if X > A:
+        print('e3')
         X -= 1
 
     if Y > Y:
+        print('e4')
         Y -= 1
+
+print(X - Y, 'answer')
