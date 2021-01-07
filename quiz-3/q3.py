4f07f6ae5b9b6c1081231dd9f36a92d773da4419
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2021-01-07 11:45:36 +0530

    Update q3.py

--- quiz-3/q3.py
+++ quiz-3/q3.py
@@ -1,3 +1,4 @@
+# dict of important roman numerals
 ROMAN_NUM = {
     1000: 'M',
     900: 'CM',
@@ -13,15 +14,17 @@ ROMAN_NUM = {
     1: 'I'
 }
 
+# placeholder
 roman_val = ''
+# input of desired number
 num = int(input("Enter your desired number > "))
-
+# the format of roman numerals changes from 4000, so keep the range till 3999
 if num < 1 or num > 3999:
     sys.exit('Cannot find the roman numeral for the given input.')
-
+# Build the roman numeral
 for roman in ROMAN_NUM.keys():
     while num >= roman:
         roman_val += ROMAN_NUM[roman]
         num -= roman
-
+# print the final result
 print(f"The roman numeral for {num} is {roman_val}")
