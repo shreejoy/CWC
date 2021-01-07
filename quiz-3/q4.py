27826ed8adeeb2c8b5bf70c7d3f015644acfc768
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2021-01-07 11:58:29 +0530

    Update q4.py

--- quiz-3/q4.py
+++ quiz-3/q4.py
@@ -1,7 +1,14 @@
+import sys
+# take the input of an odd number
 num = int(input("Enter the number (odd) > "))
+# only odd numbers are accepted
+if num % 2 == 0:
+    sys.exit("Only odd numbers are accepted")
+
+# get the range of numbers
 r = num // 2 + 1
 
-
+# function to generate pattern
 def genp(n, s):
     p = ' ' * s
 
@@ -13,12 +20,17 @@ def genp(n, s):
     return p
 
 
+# placeholder
 pattern = []
+
+# assign the max range and the needed spaces
 for i in range(r):
     spaces = (num - r - i)
     pattern.append(genp(i + 1, spaces))
 
+# join the pattern with 1 and its reverse respectively 
 pattern += pattern[:-1][::-1]
 
+# print the pattern
 for p in pattern:
     print(p)
