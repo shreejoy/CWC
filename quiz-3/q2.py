2ac8c806969a32af55b4219cd0ab5309f630c994
Shreejoy Dash <1941012973.g.shreejoydash@gmail.com>
2021-01-07 11:43:44 +0530

    Update q2.py

--- quiz-3/q2.py
+++ quiz-3/q2.py
@@ -1,10 +1,14 @@
+# take input of the first string 
 str1 = input("Enter the first string > ")
+# take input of the second string
 str2 = input("Enter the second string > ")
-
+# sort the second string and remove duplicates
 str2 = sorted(set(str2))
-
+# remove the sorted characters of str2 from st2
 for s in str2:
+    # lowercase
     str1 = str1.replace(s.lower(), '')
+    # uppercase
     str1 = str1.replace(s.upper(), '')
-
-print(str1)
+# print the result
+print("The final string is >", str1)
